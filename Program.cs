8a721dc20a9a421f492f1f73f2141be8b607b76f
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 11:39:04 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+int numb;
+Console.Write("Enter some integer value: ");
+bool is_parsed = Int32.TryParse(Console.ReadLine(), out numb);
+
+if (is_parsed && numb>=1)
+{
+    
+	for (int i = 1; i <=numb; i++)
+	{
+        Console.WriteLine($"{i} cubed is: {Math.Pow(i,3)}");
+
+    }
+}
+else {
+    Console.WriteLine( "Invalid value!!!!");
+}
+
